625933927e730831a6f8144daaf62af916c7e5fe
REV623 <kritsadakul@gmail.com>
2023-03-12 00:15:04 +0700

    - Plan finish

--- src/AST/Plan.java
+++ src/AST/Plan.java
@@ -2,25 +2,15 @@ package AST;
 
 import AST.Statement.Statement;
 import ErrorExcep.EvalError;
-import ErrorExcep.SyntaxError;
-import Model.Player;
+import GameState.Player;
 
 import java.util.LinkedList;
 
 public class Plan implements Node{
     private LinkedList<Statement> statementList;
 
-    public Plan(LinkedList<Statement> list) throws SyntaxError {
-        if(list.isEmpty()) throw new SyntaxError("construction plan is empty");
+    public Plan(LinkedList<Statement> list){
         this.statementList = list;
-    }
-
-    public Plan() {
-        this.statementList = new LinkedList<>();
-    }
-
-    public void add(Statement statement){
-        this.statementList.add(statement);
     }
 
     public void prettyPrint(StringBuilder s) {
