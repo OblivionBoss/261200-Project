9f0f4a525d922b1a8dac0fc07b1a094845899983
OblivionBoss <90751468+OblivionBoss@users.noreply.github.com>
2023-03-14 00:42:40 +0700

    Delete frontend directory

--- frontend/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/UPBEAT":["static\u002Fchunks\u002Fpages\u002FUPBEAT.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FUPBEAT","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
